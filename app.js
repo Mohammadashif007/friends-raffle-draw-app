ef626547c87e8e624b4a969311e53d39ee19694a
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-12 22:47:13 +0600

    random name generated

--- app.js
+++ app.js
@@ -17,58 +17,57 @@ window.onload = function () {
         }
     });
 
-
-
     const participantName = [];
     inputField.addEventListener("keypress", (event) => {
-        if(event.key == 'Enter'){
-            let newName = event.target.value.split(', ');
-            if(newName[0] !== ""){
-                newName.forEach(name => {
+        if (event.key == "Enter") {
+            let newName = event.target.value.split(", ");
+            if (newName[0] !== "") {
+                newName.forEach((name) => {
                     participantName.push(name);
                     let item = createListItem(name);
                     toggleDiv.appendChild(item);
                     event.target.value = "";
-                })
+                });
             }
         }
-    })
+    });
 
-    giveATry.addEventListener('click', () => {
-        if(participantName.length == 0){
-            alert("Please input some name first");
+    giveATry.addEventListener("click", () => {
+        if (participantName.length == 0) {
+            alert("Array is empty");
+        } else {
+            let shuffledName = shuffle(participantName);
+            for(let i = 1; i < shuffledName.length; i++){
+                (function(i, count){
+                    setTimeout(() => {
+                        let rand = Math.floor(Math.random()*(shuffledName.length));
+                        display.innerHTML = shuffledName[rand];
+                    }, i)
+                })(i*100, i)
+            }
         }
-        else {
-            let shuffleName = shuffle(participantName);
-        }
-    })
-
+    });
 };
 
-
-
-function createListItem(name){
-    let p = document.createElement('p');
-    p.className = 'border p-4 mb-2';
+function createListItem(name) {
+    let p = document.createElement("p");
+    p.className = "border p-4 mb-2";
     p.innerHTML = name;
     return p;
 }
 
-
 function shuffle(arr){
-    let shuffledArr = [...arr];
-    for(let i = shuffledArr.length-1; i > 0; i--){
-        let rand = Math.floor(Math.random() * (i+1));
-        let temp = shuffledArr[rand];
-        shuffledArr[rand] = shuffledArr[i];
-        shuffledArr[i] = temp;
+    const shuffledArray = [...arr];
+    for(let i= shuffledArray.length - 1; i > 0; i--){
+        let rand = Math.floor(Math.random()*(i+1));
+        let tamp = shuffledArray[rand];
+        shuffledArray[rand] = shuffledArray[i];
+        shuffledArray[i] = tamp;
     }
-    return shuffledArr
+    return shuffledArray;
 }
 
 
-// console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 // console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 // console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 // console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]));
